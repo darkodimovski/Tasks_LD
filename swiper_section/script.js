8d3ab8434e10fdd6ec8fc16ee21f6ea3dfe43989
darkodimovski <darkodimovski57@gmail.com>
2025-06-25 18:31:12 +0200

    swiper section breakpoint

---
+++ swiper_section/script.js
@@ -0,0 +1,52 @@
+class CustomSwiperController {
+  constructor(containerSelector) {
+    this.container = document.querySelector(containerSelector);
+    this.swiperInstance = null;
+  }
+
+  init() {
+    if (!this.swiperInstance) {
+      this.swiperInstance = new Swiper(this.container, {
+        slidesPerView: 1, // default for mobile
+        spaceBetween: 8,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          375: {
+            slidesPerView: 3,
+          }
+        },
+        on: {
+          slideChange: () => {
+            console.log('Active slide index:', this.swiperInstance.activeIndex);
+          }
+        }
+      });
+      console.log('Swiper initialized.');
+    }
+  }
+
+  destroy() {
+    if (this.swiperInstance) {
+      this.swiperInstance.destroy(true, true);
+      this.swiperInstance = null;
+      console.log('Swiper destroyed.');
+    }
+  }
+
+  toggle() {
+    this.swiperInstance ? this.destroy() : this.init();
+  }
+}
+
+const swiperController = new CustomSwiperController('.swiper');
+swiperController.init();
+
+document.querySelector('.toggle-swiper').addEventListener('click', () => {
+  swiperController.toggle();
+});
